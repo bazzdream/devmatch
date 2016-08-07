1d92a6226747de339c67896cfd76454f75a3ffd3
Oliver Barabas <bazzdream@gmail.com>
2016-08-07 12:03:46 +0200

    Added db seed for default plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# fill plans table with default plans
+Plan.create(name: 'Basic', price: 0)
+Plan.create(name: 'Pro', price: 10)
